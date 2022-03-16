c0f7f00c4f950e33a16e33cb2ffeccc5cad91b3f
OliverSoucc <oliv572v@easv365.dk>
2022-03-16 17:57:14 +0100

    starting with Customer controller

--- src/com/project/gui/controller/CustomerController.java
+++ src/com/project/gui/controller/CustomerController.java
@@ -1,4 +1,29 @@
 package com.project.gui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.util.Date;
+
 public class CustomerController {
+    @FXML
+    private TableView<Event> upcomingTable, purchasedTable;
+    @FXML
+    private TableColumn<Event, String> upcomingNameEventColumn, purchasedNameEventColumn;
+    @FXML
+    private TableColumn<Event, Date> upcomingStartEventColumn, purchasedStartEventColumn;
+    @FXML
+    private TableColumn<Event, String> upcomingLocationEventColumn, purchasedLocationEventColumn;
+    @FXML
+    private TableColumn<Event, String> upcomingAdditionalInfoEventColumn, purchasedAdditionalInfoEventColumn;
+
+
+    public void buyTicketOnAction(ActionEvent event) {
+    }
+
+    public void filterOnAction(ActionEvent event) {
+    }
 }
