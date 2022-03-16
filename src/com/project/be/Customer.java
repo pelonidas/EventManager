584ef94b24b9ad538f814b0c1146be00df2b9646
OliverSoucc <oliv572v@easv365.dk>
2022-03-16 18:14:43 +0100

    kk

--- src/com/project/be/Customer.java
+++ src/com/project/be/Customer.java
@@ -1,4 +1,13 @@
 package com.project.be;
 
 public class Customer extends User{
+
+
+    public Customer(int ID, String name, String email) {
+        super(ID, name, email);
+    }
+
+    public Customer(String name, String email) {
+        super(name, email);
+    }
 }
