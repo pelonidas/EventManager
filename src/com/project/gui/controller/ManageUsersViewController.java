e4e198df1767f3d9e47ba6eb599ea9f632028df4
silamin <amine.aouina.lp.4@gmail.com>
2022-03-17 22:07:40 +0100

    work in progress

--- src/com/project/gui/controller/ManageUsersViewController.java
+++ src/com/project/gui/controller/ManageUsersViewController.java
@@ -2,8 +2,13 @@ package com.project.gui.controller;
 
 import com.project.gui.view.Main;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,5 +30,27 @@ public class ManageUsersViewController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void newCustomer(ActionEvent actionEvent) throws IOException {
+        Parent root;
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/com/project/gui/view/NewUserView.fxml"));
+        root = loader.load();
+        Stage stage = new Stage();
+        stage.setTitle("New customer");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public void newUser(ActionEvent actionEvent) throws IOException {
+        Parent root;
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/com/project/gui/view/NewUserView.fxml"));
+        root = loader.load();
+        Stage stage = new Stage();
+        stage.setTitle("New user");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
