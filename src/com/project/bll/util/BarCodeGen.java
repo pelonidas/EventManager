a685028413f69132ebd80e00d4a717e3f6ccc97b
Renaldinho <rena0968@easv365.dk>
2022-03-18 10:24:12 +0100

    Update BarCodeGen.java

--- src/com/project/bll/util/BarCodeGen.java
+++ src/com/project/bll/util/BarCodeGen.java
@@ -1,6 +1,7 @@
 package com.project.bll.util;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
@@ -9,25 +10,30 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 
 import java.awt.image.BufferedImage;
+import java.util.UUID;
 
 public class BarCodeGen {
 
-    private final int QR_SIZE = 50;
-    private final int BARCODE_HEIGHT = 50;
-    private final int BARCODE_WIDTH = 120;
+    private final int QR_SIZE = 150;
+    private final int BARCODE_HEIGHT = 100;
+    private final int BARCODE_WIDTH = 100;
 
 
-    public Image generateQRCode(String information) throws WriterException {
+    public Image generateQRCode() throws WriterException {
+        String uniqueID = UUID.randomUUID().toString();
+
         QRCodeWriter qrWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrWriter.encode(information, BarcodeFormat.QR_CODE,QR_SIZE,QR_SIZE);
+        BitMatrix bitMatrix = qrWriter.encode(uniqueID, BarcodeFormat.QR_CODE,QR_SIZE,QR_SIZE);
 
         BufferedImage qrImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
         return SwingFXUtils.toFXImage(qrImage,null);
     }
 
-    public Image generateBarCode(String information) throws WriterException {
-        QRCodeWriter qrWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrWriter.encode(information, BarcodeFormat.CODE_128,BARCODE_WIDTH,BARCODE_HEIGHT);
+    public Image generateBarCode() throws WriterException {
+        String uniqueID = UUID.randomUUID().toString();
+
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        BitMatrix bitMatrix = multiFormatWriter.encode(uniqueID, BarcodeFormat.CODE_39,BARCODE_WIDTH,BARCODE_HEIGHT);
 
         BufferedImage barcodeImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
         return SwingFXUtils.toFXImage(barcodeImage,null);
