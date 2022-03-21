b61a66314fa7d98732562545ecf3af9213624738
pelonidas <janx803y@easv365.dk>
2022-03-21 21:40:26 +0100

    CoordinatorView update

--- src/com/project/gui/controller/CoordinatorController.java
+++ src/com/project/gui/controller/CoordinatorController.java
@@ -1,4 +1,15 @@
 package com.project.gui.controller;
 
-public class CoordinatorController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CoordinatorController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
