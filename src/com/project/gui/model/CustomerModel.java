a3df50db5482de9ee954e1f375ea11971fd4cec2
OliverSoucc <oliv572v@easv365.dk>
2022-03-22 22:43:11 +0100

    gg

--- src/com/project/gui/model/CustomerModel.java
+++ src/com/project/gui/model/CustomerModel.java
@@ -1,26 +1,19 @@
 package com.project.gui.model;
 
 import com.microsoft.sqlserver.jdbc.SQLServerException;
-import com.project.be.Coordinator;
-import com.project.be.Customer;
-import com.project.be.Event;
-import com.project.be.User;
+
 import com.project.bll.EventManager;
 import com.project.bll.IEventManager;
 import com.project.dal.facadeDAO.DALController;
 import com.project.dal.facadeDAO.IDALFacade;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+
 
 import java.io.IOException;
-import java.sql.Date;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
+
 
 public class CustomerModel {
     IDALFacade EMFacade;
