0e8b6c86b1ebe4c19cd145a109e32d6f129d6f89
pelonidas <janx803y@easv365.dk>
2022-04-16 12:42:17 +0200

    css updates

--- src/com/project/gui/view/TempMain.java
+++ src/com/project/gui/view/TempMain.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -19,16 +20,18 @@ public class TempMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Font.loadFont(
+                TempMain.class.getResource("../resources/fonts/Comfortaa-Regular.ttf").toExternalForm(), 10
+        );
         Parent root = FXMLLoader.load(getClass().getResource("CoordinatorView.fxml"));
-        try {
-            GraphicsEnvironment ge =
-                    GraphicsEnvironment.getLocalGraphicsEnvironment();
-            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("../resources/fonts/Comfortaa-Regular.ttf")));
-        } catch (IOException | FontFormatException e) {
-            //Handle exception
-            System.out.println("Font couldn't be loaded");
-        }
+//        try {
+//            GraphicsEnvironment ge =
+//                    GraphicsEnvironment.getLocalGraphicsEnvironment();
+//            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("../resources/fonts/Comfortaa-Regular.ttf")));
+//        } catch (IOException | FontFormatException e) {
+//            //Handle exception
+//            System.out.println("Font couldn't be loaded");
+//        }
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
